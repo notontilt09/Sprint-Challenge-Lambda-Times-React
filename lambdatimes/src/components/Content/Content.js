86873612c9d21a319d749aec288d7d53da33cb7a
Daniel Weinman <namniewd@gmail.com>
2019-01-18 11:31:02 -0500

    data flow from content -> cards -> card complete

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -56,7 +56,7 @@ export default class Content extends Component {
           and `selectTabHandler` that includes the function to change the selected tab
         */}
         <Tabs tabs={this.state.tabs} selectedTab={this.state.selected} selectTabHandler={this.changeSelected} />
-        <Cards cards={this.filterCards()} />
+        <Cards cards={this.state.cards} />
       </div>
     );
   }
