8c3d8bb616714567de5b406df7d78cf87ef414c5
Daniel Weinman <namniewd@gmail.com>
2019-01-18 13:56:45 -0500

    converted almost all components into styled components without css

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,19 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const TabTab = styled.div`
+  display: flex;
+  justify-content: none;
+  align-items: center;
+  flex-direction: row;
+  color: #fff;
+  background-color: #333;
+  margin: 0 5px;
+  padding: 2px 10px;
+  font-size: 12px;
+  letter-spacing: 2px;
+  cursor: pointer;
+  font-weight: bold;
+`;
 
 const Tab = props => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
   return (
-    <div
-      className={`tab ${props.tab === props.selectedTab && 'active-tab'}`}
+    <TabTab
+      className={`${props.tab === props.selectedTab && 'active-tab'}`}
       onClick={() => 
           props.selectTabHandler(props.tab)
         }
     >
       {props.tab.toUpperCase()}
-    </div>
+    </TabTab>
   );
 };
 
--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,12 +1,43 @@
 import React from 'react';
+import styled from 'styled-components';
 import Tab from './Tab';
 import PropTypes from 'prop-types';
 
+const TabsContainer = styled.div`
+  border-bottom: 1px solid lightgrey;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  width: 100%;
+  height: 47px;
+  background-color: #fff;
+
+  @media (min-width: 1280px) {
+      width: 1280px;
+  }
+`;
+
+const Topics = styled.div`
+  display: flex;
+  justify-content: none;
+  align-items: center;
+  flex-direction: row;
+`;
+
+const Title = styled.span`
+  font-size: 12px;
+  letter-spacing: 1px;
+  font-weight: bold;
+`;
+
+
+
 const Tabs = props => {
   return (
-    <div className="tabs">
-      <div className="topics">
-        <span className="title">TRENDING TOPICS:</span>
+    <TabsContainer>
+      <Topics>
+        <Title>TRENDING TOPICS:</Title>
         {/* map over the tabs provided on your props, create a new Tab component for each one.
             give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
             {props.tabs.map((tab, index) => 
@@ -18,8 +49,8 @@ const Tabs = props => {
                 filterCards={props.filterCards}
               />
             )}
-      </div>
-    </div>
+      </Topics>
+    </TabsContainer>
   );
 };
 
