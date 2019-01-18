e13de38a6d7ffdd7553488b0db1c61213a5b6407
Daniel Weinman <namniewd@gmail.com>
2019-01-18 13:31:20 -0500

    implemented carousel of images

--- lambdatimes/src/App.js
+++ lambdatimes/src/App.js
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 import TopBar from './components/TopBar';
 import Header from './components/Header';
 import Content from './components/Content/Content';
-import authenticate from './components/Authentication/authenticate'
-import Login from './components/Login'
+import authenticate from './components/Authentication/authenticate';
+import Login from './components/Login';
+import Carousel from './components/Carousel/Carousel'
 
 class App extends Component {
   logout() {
@@ -16,6 +17,7 @@ class App extends Component {
       <div className="App">
         <TopBar logout={this.logout}/>
         <Header />
+        <Carousel />
         <Content />
       </div>
     );
--- lambdatimes/src/components/Carousel/Carousel.js
+++ lambdatimes/src/components/Carousel/Carousel.js
@@ -1,35 +1,60 @@
 import React, { Component } from 'react';
 import { carouselData } from '../../data'
 // Complete this Carousel 
-export default class Carousel extends Component {
+
+class Carousel extends Component {
   constructor(props){
     super(props);
     this.state = {
-
+      carousel: [],
+      selected: null
     }
   }
   componentDidMount(){
-
+    this.setState({
+      carousel: carouselData,
+      selected: 0
+    })
   }
 
   leftClick = () => {
+    if (this.state.selected === 0) {
+      this.setState({
+        selected: this.state.carousel.length -1
+      })
+    } else {
+      this.setState({
+        selected: this.state.selected - 1
+      })
+    }
 
   }
 
   rightClick = () => {
-
+    if (this.state.selected === (this.state.carousel.length - 1)) {
+      this.setState({
+        selected: 0
+      })
+    } else {
+      this.setState({
+        selected: this.state.selected + 1
+      })
+    }
   }
 
   selectedImage = () => {
-    return <img src={} style={{display: 'block'}} />
+    return <img src={this.state.carousel[this.state.selected]} alt='carousel' style={{display: 'block'}} />
   }
   
   render(){
     return (
       <div className="carousel">
         <div className="left-button" onClick={this.leftClick}>{"<"}</div>
+        <div className="selected-Image">{this.selectedImage()}</div>
         <div className="right-button" onClick={this.rightClick}>{">"}</div>
       </div>
     )
   }
 }
+
+export default Carousel;
