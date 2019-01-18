0da73ec199878be5c16d414d6ece72b0457292fe
Daniel Weinman <namniewd@gmail.com>
2019-01-18 13:41:22 -0500

    refactored Carousel to use only styled components

--- lambdatimes/src/components/Carousel/Carousel.js
+++ lambdatimes/src/components/Carousel/Carousel.js
@@ -1,6 +1,78 @@
 import React, { Component } from 'react';
-import { carouselData } from '../../data'
+import { carouselData } from '../../data';
+import styled from 'styled-components';
 // Complete this Carousel 
+
+const CarouselContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  height: 500px;
+  position: relative;
+  overflow: hidden;
+  margin-top: 16px;
+
+  @media (min-width: 1200px) {
+      width: 1200px;
+  }
+
+  img {
+    width: 100%;
+    display: none;
+  }
+`;
+
+const LeftButton = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: none;
+  flex-direction: row;
+  color: #fff;
+  background-color: #333;
+  font-size: 40px;
+  border-radius: 50%;
+  position: absolute;
+  width: 50px;
+  height: 50px;
+  cursor: pointer;
+  top: 50%;
+  left: 25px;
+  transform: translate(0, -50%);
+
+  &: hover {
+    color: #333;
+    background-color: #fff;
+    border: 2px solid #333;
+  }
+`;
+
+const SelectedImage = styled.div``;
+
+const RightButton = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: none;
+  flex-direction: row;
+  color: #fff;
+  background-color: #333;
+  font-size: 40px;
+  border-radius: 50%;
+  position: absolute;
+  width: 50px;
+  height: 50px;
+  cursor: pointer;
+  top: 50%;
+  right: 25px;
+  transform: translate(0, -50%);
+
+  &: hover {
+    color: #333;
+    background-color: #fff;
+    border: 2px solid #333;
+  }
+`;
 
 class Carousel extends Component {
   constructor(props){
@@ -48,11 +120,11 @@ class Carousel extends Component {
   
   render(){
     return (
-      <div className="carousel">
-        <div className="left-button" onClick={this.leftClick}>{"<"}</div>
-        <div className="selected-Image">{this.selectedImage()}</div>
-        <div className="right-button" onClick={this.rightClick}>{">"}</div>
-      </div>
+      <CarouselContainer>
+        <LeftButton onClick={this.leftClick}>{"<"}</LeftButton>
+        <SelectedImage>{this.selectedImage()}</SelectedImage>
+        <RightButton onClick={this.rightClick}>{">"}</RightButton>
+      </CarouselContainer>
     )
   }
 }
